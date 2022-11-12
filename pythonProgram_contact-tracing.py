c63c74ac328ecf643516b1ecc141b795609909c5
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-11-12 21:49:39 +0800

    Coded: Menu List

--- pythonProgram_contact-tracing.py
+++ pythonProgram_contact-tracing.py
@@ -3,7 +3,18 @@ print("PROGRAMMED BY: Crystal Jane Cadimas")
 print("Course/Year:   BSCOE 2-2")
 
 # Display a menu option 
-# Allow the user to selct an item in the menu (check if valid)
+print()
+print("================ MENU LIST ================\n")
+print(" What do you want to do?\n")
+print(" [a] Add a contact")
+print(" [b] Change a contact")
+print(" [c] Search a contact")
+print(" [d] Delete a contact")
+print(" [e] Delete all contacts")
+print(" [f] Display contact's length")
+print(" [g] Exit program\n")
+
+# Allow the user to select an item in the menu (check if valid)
 # Perform the selected option
 # Oprion 1: Ask personal data for contact tracing to store the info
 # Option 2: Search, ask full name then display the record
