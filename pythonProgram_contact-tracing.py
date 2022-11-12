e9951a36bf18b2ac465660e330430ce24badeb2c
Crystal Jane Cadimas <crystaljanecadimas@iskolarngbayan.pup.edu.ph>
2022-11-12 21:11:09 +0800

    Inputted Guidelines

--- pythonProgram_contact-tracing.py
+++ pythonProgram_contact-tracing.py
@@ -2,4 +2,9 @@ print("\n")
 print("PROGRAMMED BY: Crystal Jane Cadimas")
 print("Course/Year:   BSCOE 2-2")
 
-
+# Display a menu option 
+# Allow the user to selct an item in the menu (check if valid)
+# Perform the selected option
+# Oprion 1: Ask personal data for contact tracing to store the info
+# Option 2: Search, ask full name then display the record
+# Option 3: Ask the user if s/he wants to exit or retry
